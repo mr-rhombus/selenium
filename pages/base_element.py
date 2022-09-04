e400bec93d7fbc8f0c149dc3f920f8d8a24adb80
Max Diamond <diamax98@gmail.com>
2022-09-03 20:19:46 -0400

    Add module with generic class for all elements on a page

---
+++ pages/base_element.py
@@ -0,0 +1,32 @@
+# Wrap a Selenium web element, then allow it to do some "fancier" things
+
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BaseElement:
+    def __init__(self, driver, by, value):
+        self.driver = driver
+        self.value = value
+        self.by = by
+        # Locator arg in Selenium expects both "By" and "Locator value" in a tuple
+        self.locator = (self.by, self.value)
+
+        self.web_element = None
+        self.find()
+
+    def find(self):
+        # Wait up to 10s for the element to be located
+        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.locator))
+        # self.driver.find_element(self.by, self.locator)  # Unsophisticated way
+        self.web_element = element
+
+    def click(self):
+        # Make sure element (button) is clickable before clicking it
+        element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.locator))
+        element.click()
+
+    # Treat this fn like a property, allowing us to get/set values
+    @property
+    def text(self):
+        text = self.web_element.text
+        return text
